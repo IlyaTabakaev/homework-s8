a2646ce835784e92a0fd6a12ea7d0f9c5d72bbe9
Ilya <osetia9595@gmail.com>
2023-01-16 17:41:49 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("input rows count");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("input columns count");
+int n = int.Parse(Console.ReadLine());
+int [,] array = new int [m, n];
+FillArray(array);
+PrintArray(array);
+MinimalRowSum(array);
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100);
+            }   
+        }
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.WriteLine(""); 
+    }
+}
+
+void MinimalRowSum(int[,] array)
+{
+    int minRow = 0;
+    int minRowSum = 0;
+    int rowSum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) rowSum = array[i, j];
+        if (rowSum < minRow)
+        {
+            minRow = rowSum;
+            minRowSum = i;
+        }
+        rowSum = 0;
+    }
+    Console.Write($"{minRowSum + 1} line with minimal sum");
+}
