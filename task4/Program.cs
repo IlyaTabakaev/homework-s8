6b326e865d2cd4f40ba651d6ea34d439ef34a0c6
Ilya <osetia9595@gmail.com>
2023-01-16 22:32:03 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,,] array = new int[3, 3, 3];
+FillArray(array);
+
+PrintArray(array);
+
+void FillArray(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
